29fabc5b4bf2ce2e1a081a47200d5201f95de6d3
abbas67 <androidabbas@googlemail.com>
2020-03-16 16:56:05 +0000

    Implementation Complete

--- create_mock_data.py
+++ create_mock_data.py
@@ -13,8 +13,17 @@ params = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={uid};PWD={pw
 conn = pyodbc.connect(params)
 cursor = conn.cursor()
 
-query = "select top 50 MatricNum from Students"
+query = "select top 50 MatricNum, FirstName, LastName from Students"
 result = pd.read_sql(query, conn)
 result_dict = result.to_dict('records')
 
 print(result_dict)
+
+with open('MOCK_DATA.csv', 'w') as file:
+
+    file.write('MatricNum,FirstName,LastName')
+
+    for student in result_dict:
+
+        string = ('\n' + str(student['MatricNum']) + "," + student['FirstName'] + "," + student['LastName'])
+        file.write(string)
